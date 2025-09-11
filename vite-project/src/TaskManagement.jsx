e58842933734c0cedf7fd41f08efc6acd46f5879
GabGerolia <johngabrielgerolia05@gmail.com>
2025-09-11 20:49:37 +0800

    forgot to include this in last commit

--- vite-project/src/TaskManagement.jsx
+++ vite-project/src/TaskManagement.jsx
@@ -2,7 +2,9 @@ import { useState, useRef, useLayoutEffect, useEffect } from "react";
 import { DndContext, closestCorners, DragOverlay, } from "@dnd-kit/core";
 import { SortableContext, horizontalListSortingStrategy, arrayMove } from "@dnd-kit/sortable";
 import { useLocation, useParams } from "react-router-dom";
+import { threeDotsIcon, deleteIcon, editIcon } from "./constVars";
 import axios from "axios";
+
 
 import Navbar from "./Navbar";
 import TaskCard from "./TaskCard";
@@ -34,25 +36,6 @@ function TaskManagement() {
     if (!col || !col.tasks) return -1;
     return col.tasks.findIndex((t) => t.id === taskId);
   };
-
-  // icons
-  const threeDotsIcon = (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-      viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
-      className="size-6">
-      <path strokeLinecap="round" strokeLinejoin="round"
-        d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-    </svg>
-  );
-
-  const editIcon = (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-      viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
-      className="size-6">
-      <path strokeLinecap="round" strokeLinejoin="round"
-        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-    </svg>
-  );
 
   //editing project title/description
   const [showEditingProject, setShowEditingProject] = useState(false);
@@ -117,7 +100,7 @@ function TaskManagement() {
         position: columns.length,
       })
         .then(res => {
-          setColumns(prev => [...prev, res.data]);
+          setColumns(prev => [...prev, { ...res.data, tasks: [] }]); // always include empty tasks
           setShowCreateColumn(false);
         })
         .catch(err => console.error("Error saving column:", err));
