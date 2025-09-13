1c8dbf4295c71c2251141bddc6a4eeda4c2ab9e7
GabGerolia <johngabrielgerolia05@gmail.com>
2025-09-13 23:22:01 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,9 @@ const db = mysql.createConnection({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT || 3306
 });
+
 
 
 db.connect((err) => {
