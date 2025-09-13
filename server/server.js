7bb512ed11a07c03e3bd64e1bc19ddccbf51481a
GabGerolia <johngabrielgerolia05@gmail.com>
2025-09-13 22:06:16 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require("dotenv").config(); // Load environment variables first
 
 const express = require("express");
 const cors = require("cors");
-const mysql = require("mysql2");
+const mysql = require("mysql");
 
 const app = express();
 
