27912d43ae69939c6486587e73a96b4e738e7172
GabGerolia <johngabrielgerolia05@gmail.com>
2025-09-23 22:08:35 +0800

    transferred from bcrypt to bcryptjs

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,5 @@
 const db = require("../config/db");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const JWT_SECRET = process.env.JWT_SECRET || "secretKeyExample";
